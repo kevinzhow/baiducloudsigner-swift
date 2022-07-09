9927d2c1b9b02e899b41c38143280534e83b5906
kevinzhow <kevinchou.c@gmail.com>
2022-07-09 21:22:10 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BaiduCloudSigner
+
+A description of this package.
---
+++ Sources/BaiduCloudSigner/BaiduCloudSigner.swift
@@ -0,0 +1,6 @@
+public struct BaiduCloudSigner {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
